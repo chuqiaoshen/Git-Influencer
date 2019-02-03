8f6b3809239b994b58cd438ebde7b02935460438
Catherine <chuqiaoshen2017@gmail.com>
2019-02-02 19:05:16 -0800

    get barplot with data get from mysqldb

--- flask_dashboard/app.py
+++ flask_dashboard/app.py
@@ -1,1 +1,71 @@
-sample 
+"""
+Catherine Shen 20180201
+Simple dash app with barplot
+"""
+import os
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
+import pymysql
+import pymysql.cursors
+
+# initial dash app
+app = dash.Dash()
+
+#echo environment variables from instances for mysql login
+mysql_host = os.environ['mysql_host']
+mysql_username = os.environ['mysql_username']
+mysql_password = os.environ['mysql_password']
+
+# Get DATA from mysql db
+connection = pymysql.connect(host = mysql_host,
+                             user = mysql_username,
+                             password = mysql_password,
+                             db = 'rank',
+                             charset = 'utf8mb4',
+                             cursorclass = pymysql.cursors.DictCursor)
+
+cursor = connection.cursor()
+#select top 15 users by their pange rank rating
+cursor.execute("SELECT user,rank FROM user_rank ORDER BY rank DESC LIMIT 15")
+data = cursor.fetchall()
+print(data)
+user_list = []
+temp_list = []
+rank_list = []
+for i in range(len(data)):
+    user_list.append(data[i]['user'])
+    temp_list.append(i)
+    rank_list.append(float(data[i]['rank']))
+
+######################
+# in plotly the barchart show as
+'''data = [go.Bar(
+    x=user_list,  # NOC stands for National Olympic Committee
+    y=rank_list
+)]
+layout = go.Layout(
+    title='Java User Page Rank Rating top 15'
+)
+fig = go.Figure(data=data, layout=layout)
+pyo.plot(fig, filename='PageRank_Java.html')
+children = [
+                      html.H2(children = "Git-Influencer"),
+                      html.Div(children = "Catherine Shen")
+                      host='0.0.0.0', port=8080'''
+#######################
+app.layout = html.Div([dcc.Graph(id='JavaPageRank',
+                                 figure = {'data':[go.Bar(
+                                                   x=user_list,  # NOC stands for National Olympic Committee
+                                                   y=rank_list
+                                                              )],
+                                           'layout':go.Layout(
+                                title='Java User Page Rank Rating top 15',
+                                xaxis = {'title':'User github login name'})}
+                                )])
+
+
+if __name__ == '__main__':
+    app.run_server()
