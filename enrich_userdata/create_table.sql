1ea1f7738b05a431a2d6ed49571c104146778146
Catherine <chuqiaoshen2017@gmail.com>
2019-02-10 17:45:00 -0800

    create createtable.sql for back up

---
+++ enrich_userdata/create_table.sql
@@ -0,0 +1,179 @@
+CREATE TABLE Scala (
+    user_name varchar(255) NOT NULL,
+    user_rank INT,
+    user_type varchar(255),
+    github_age INT,
+    followers_num INT,
+    following_num INT,
+    repo_num INT,
+    gist_num INT,
+    location varchar(255),
+    company varchar(255),
+    PRIMARY KEY (user_name))
+ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+AUTO_INCREMENT=1 ;
+
+CREATE TABLE C (
+    user_name varchar(255) NOT NULL,
+    user_rank INT,
+    user_type varchar(255),
+    github_age INT,
+    followers_num INT,
+    following_num INT,
+    repo_num INT,
+    gist_num INT,
+    location varchar(255),
+    company varchar(255),
+    PRIMARY KEY (user_name))
+ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+AUTO_INCREMENT=1 ;
+
+CREATE TABLE Cplus (
+    user_name varchar(255) NOT NULL,
+    user_rank INT,
+    user_type varchar(255),
+    github_age INT,
+    followers_num INT,
+    following_num INT,
+    repo_num INT,
+    gist_num INT,
+    location varchar(255),
+    company varchar(255),
+    PRIMARY KEY (user_name))
+ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+AUTO_INCREMENT=1 ;
+
+CREATE TABLE Csharp (
+    user_name varchar(255) NOT NULL,
+    user_rank INT,
+    user_type varchar(255),
+    github_age INT,
+    followers_num INT,
+    following_num INT,
+    repo_num INT,
+    gist_num INT,
+    location varchar(255),
+    company varchar(255),
+    PRIMARY KEY (user_name))
+ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+AUTO_INCREMENT=1 ;
+
+CREATE TABLE Go (
+    user_name varchar(255) NOT NULL,
+    user_rank INT,
+    user_type varchar(255),
+    github_age INT,
+    followers_num INT,
+    following_num INT,
+    repo_num INT,
+    gist_num INT,
+    location varchar(255),
+    company varchar(255),
+    PRIMARY KEY (user_name))
+ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+AUTO_INCREMENT=1 ;
+
+CREATE TABLE Java (
+    user_name varchar(255) NOT NULL,
+    user_rank INT,
+    user_type varchar(255),
+    github_age INT,
+    followers_num INT,
+    following_num INT,
+    repo_num INT,
+    gist_num INT,
+    location varchar(255),
+    company varchar(255),
+    PRIMARY KEY (user_name))
+ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+AUTO_INCREMENT=1 ;
+
+CREATE TABLE JavaScript (
+    user_name varchar(255) NOT NULL,
+    user_rank INT,
+    user_type varchar(255),
+    github_age INT,
+    followers_num INT,
+    following_num INT,
+    repo_num INT,
+    gist_num INT,
+    location varchar(255),
+    company varchar(255),
+    PRIMARY KEY (user_name))
+ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+AUTO_INCREMENT=1 ;
+
+CREATE TABLE Perl (
+    user_name varchar(255) NOT NULL,
+    user_rank INT,
+    user_type varchar(255),
+    github_age INT,
+    followers_num INT,
+    following_num INT,
+    repo_num INT,
+    gist_num INT,
+    location varchar(255),
+    company varchar(255),
+    PRIMARY KEY (user_name))
+ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+AUTO_INCREMENT=1 ;
+
+CREATE TABLE PHP (
+    user_name varchar(255) NOT NULL,
+    user_rank INT,
+    user_type varchar(255),
+    github_age INT,
+    followers_num INT,
+    following_num INT,
+    repo_num INT,
+    gist_num INT,
+    location varchar(255),
+    company varchar(255),
+    PRIMARY KEY (user_name))
+ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+AUTO_INCREMENT=1 ;
+
+CREATE TABLE Python (
+    user_name varchar(255) NOT NULL,
+    user_rank INT,
+    user_type varchar(255),
+    github_age INT,
+    followers_num INT,
+    following_num INT,
+    repo_num INT,
+    gist_num INT,
+    location varchar(255),
+    company varchar(255),
+    PRIMARY KEY (user_name))
+ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+AUTO_INCREMENT=1 ;
+
+CREATE TABLE Ruby (
+    user_name varchar(255) NOT NULL,
+    user_rank INT,
+    user_type varchar(255),
+    github_age INT,
+    followers_num INT,
+    following_num INT,
+    repo_num INT,
+    gist_num INT,
+    location varchar(255),
+    company varchar(255),
+    PRIMARY KEY (user_name))
+ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+AUTO_INCREMENT=1 ;
+
+CREATE TABLE Shell (
+    user_name varchar(255) NOT NULL,
+    user_rank INT,
+    user_type varchar(255),
+    github_age INT,
+    followers_num INT,
+    following_num INT,
+    repo_num INT,
+    gist_num INT,
+    location varchar(255),
+    company varchar(255),
+    PRIMARY KEY (user_name))
+ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+AUTO_INCREMENT=1 ;
